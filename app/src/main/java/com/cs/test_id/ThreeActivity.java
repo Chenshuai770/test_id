d288290d0f372ec80401239cbcffefacd0b1f1bb
chenshuai <770456288@qq.com>
2020-02-06 16:45:51 +0800

    Initial commit

---
+++ app/src/main/java/com/cs/test_id/ThreeActivity.java
@@ -0,0 +1,152 @@
+package com.cs.test_id;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.ocr.sdk.OCR;
+import com.baidu.ocr.sdk.OnResultListener;
+import com.baidu.ocr.sdk.exception.OCRError;
+import com.baidu.ocr.sdk.model.AccessToken;
+import com.baidu.ocr.sdk.model.IDCardParams;
+import com.baidu.ocr.sdk.model.IDCardResult;
+import com.baidu.ocr.ui.camera.CameraActivity;
+import com.baidu.ocr.ui.camera.CameraNativeHelper;
+import com.baidu.ocr.ui.camera.CameraView;
+
+import java.io.File;
+
+public class ThreeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnTest;
+    private static final int REQUEST_CODE_CAMERA = 102;
+    private TextView mTvTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+        initAccessToken();
+        initView();
+    }
+
+    private void initView() {
+
+        mBtnTest = (Button) findViewById(R.id.btn_test);
+        mTvTest = (TextView) findViewById(R.id.tv_test);
+
+        mBtnTest.setOnClickListener(this);
+        //  初始化本地质量控制模型,释放代码在onDestory中
+        //  调用身份证扫描必须加上 intent.putExtra(CameraActivity.KEY_NATIVE_MANUAL, true); 关闭自动初始化和释放本地模型
+        CameraNativeHelper.init(this, OCR.getInstance(this).getLicense(),
+                new CameraNativeHelper.CameraNativeInitCallback() {
+                    @Override
+                    public void onError(int errorCode, Throwable e) {
+                        String msg;
+                        switch (errorCode) {
+                            case CameraView.NATIVE_SOLOAD_FAIL:
+                                msg = "加载so失败，请确保apk中存在ui部分的so";
+                                break;
+                            case CameraView.NATIVE_AUTH_FAIL:
+                                msg = "授权本地质量控制token获取失败";
+                                break;
+                            case CameraView.NATIVE_INIT_FAIL:
+                                msg = "本地质量控制";
+                                break;
+                            default:
+                                msg = String.valueOf(errorCode);
+                        }
+                        //infoTextView.setText("本地质量控制初始化错误，错误原因： " + msg);
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_test:
+                Intent intent = new Intent(this, CameraActivity.class);
+                intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH,
+                        FileUtil.getSaveFile(getApplication()).getAbsolutePath());
+                intent.putExtra(CameraActivity.KEY_NATIVE_ENABLE,
+                        true);
+                // KEY_NATIVE_MANUAL设置了之后CameraActivity中不再自动初始化和释放模型
+                // 请手动使用CameraNativeHelper初始化和释放模型
+                // 推荐这样做，可以避免一些activity切换导致的不必要的异常
+                intent.putExtra(CameraActivity.KEY_NATIVE_MANUAL,
+                        true);
+                intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_ID_CARD_FRONT);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+                break;
+        }
+    }
+    /**
+     * 以license文件方式初始化
+     */
+    private void initAccessToken() {
+        OCR.getInstance(this).initAccessToken(new OnResultListener<AccessToken>() {
+            @Override
+            public void onResult(AccessToken accessToken) {
+                String token = accessToken.getAccessToken();
+                // hasGotToken = true;
+            }
+
+            @Override
+            public void onError(OCRError error) {
+                error.printStackTrace();
+               // alertText("licence方式获取token失败", error.getMessage());
+            }
+        }, getApplicationContext());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                String contentType = data.getStringExtra(CameraActivity.KEY_CONTENT_TYPE);
+                String filePath = FileUtil.getSaveFile(getApplicationContext()).getAbsolutePath();
+                if (!TextUtils.isEmpty(contentType)) {
+                    if (CameraActivity.CONTENT_TYPE_ID_CARD_FRONT.equals(contentType)) {
+                        recIDCard(IDCardParams.ID_CARD_SIDE_FRONT, filePath);
+                    } else if (CameraActivity.CONTENT_TYPE_ID_CARD_BACK.equals(contentType)) {
+                        recIDCard(IDCardParams.ID_CARD_SIDE_BACK, filePath);
+                    }
+                }
+            }
+        }
+    }
+
+    private void recIDCard(String idCardSide, String filePath) {
+        IDCardParams param = new IDCardParams();
+        param.setImageFile(new File(filePath));
+        // 设置身份证正反面
+        param.setIdCardSide(idCardSide);
+        // 设置方向检测
+        param.setDetectDirection(true);
+        // 设置图像参数压缩质量0-100, 越大图像质量越好但是请求时间越长。 不设置则默认值为20
+        param.setImageQuality(20);
+
+        OCR.getInstance(this).recognizeIDCard(param, new OnResultListener<IDCardResult>() {
+            @Override
+            public void onResult(IDCardResult result) {
+                if (result != null) {
+                    mTvTest.setText(result.getName()+"\n"+result.getIdNumber());
+                }
+            }
+
+            @Override
+            public void onError(OCRError error) {
+                //alertText("", error.getMessage());
+            }
+        });
+    }
+}
